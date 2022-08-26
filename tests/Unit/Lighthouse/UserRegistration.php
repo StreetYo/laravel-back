938086fbad1abe5bcf614aeda3bb8df3adf1c96c
Nariman <nngasanov@gmail.com>
2022-08-27 02:59:26 +0500

    Add UserRegistration test

---
+++ tests/Unit/Lighthouse/UserRegistration.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Unit\Lighthouse;
+
+use Illuminate\Foundation\Testing\TestCase;
+use Nuwave\Lighthouse\Testing\MakesGraphQLRequests;
+use Nuwave\Lighthouse\Testing\RefreshesSchemaCache;
+use Tests\CreatesApplication;
+
+class UserRegistration extends TestCase
+{
+    use CreatesApplication;
+    use MakesGraphQLRequests;
+    use RefreshesSchemaCache;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->bootRefreshesSchemaCache();
+    }
+
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_registration()
+    {
+        $this->graphQL(/** @lang GraphQL */
+    'mutation {
+              register(
+                  username: "stretstreet",
+                  name: "Nariman",
+                  email: "stretstreet@yandex.ru",
+                  password: "qwerty"
+              ) {
+                id
+                name
+                email
+              }
+        }')
+        ->assertJson([
+            'data' => [
+                'register' => [
+                    [
+                        'id' => '1',
+                        'name' => 'Nariman',
+                        'email' => 'stretstreet@yandex.ru',
+                    ],
+                ],
+            ],
+        ]);
+    }
+}
